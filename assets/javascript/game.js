6074d5acc5d65e8cedf60416ec7cf5542e980f5f
Eva <ol0.badwolf.0lo@gmail.com>
2017-09-13 22:47:53 -0400

    Updated JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,7 +11,11 @@
 // write the letter the computer chooses to the console that way you can make sure that it's working. 
 
 
+//Global Variables//
 
+var numberOfGuesses = 7;
+var wins = 0;
+var losses = 0;
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 function getRndLetter() {
@@ -28,10 +32,17 @@ document.onkeyup = function(){
 
 	if (userPick == compPick) {
 		alert("Congratulations! You win!");
+		wins++;
+		// console.log(wins);
+		document.querySelector("#wins").innerText = "Wins: " + wins;
+		compPick = getRndLetter()
+		console.log(compPick);
 	}
 
 	else {
-		alert("Wrong Letter! Please try again!")
+		alert("Wrong Letter! Please try again!");
+		document.querySelector("#guesses").innerText = "Your Last Was: " + userPick;
+
 	}
 }
 
