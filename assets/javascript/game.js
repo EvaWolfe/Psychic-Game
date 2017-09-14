d7eb3a9dd3676db327525ed2c693f83cd2904ae6
Eva <ol0.badwolf.0lo@gmail.com>
2017-09-13 22:18:20 -0400

    Updated JavaScript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,38 @@
-computer has to randomly choose a letter
-user has to be able to type the letter they want to pick
-keep track of wins 
-keep track of losses
-keep track of the letters guessed
+// computer has to randomly choose a letter
+// user has to be able to type the letter they want to pick
+// keep track of wins 
+// keep track of losses
+// keep track of the letters guessed
 
-tell the computer what letters it can choose
+// tell the computer what letters it can choose
 
-make sure that the letters are not case sensitive
+// make sure that the letters are not case sensitive
 
-write the letter the computer chooses to the console that way you can make sure that it's working. 
+// write the letter the computer chooses to the console that way you can make sure that it's working. 
 
 
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-function getRndLetter {
+function getRndLetter() {
 	var i = Math.floor(Math.random() * 26)
 	return letters[i]
 }
 
-var hold = getRndLetter
+var compPick = getRndLetter()
+console.log(compPick);
+
+document.onkeyup = function(){
+	var userPick = String.fromCharCode(event.keyCode).toLowerCase();
+	console.log(userPick);
+
+	if (userPick == compPick) {
+		alert("Congratulations! You win!");
+	}
+
+	else {
+		alert("Wrong Letter! Please try again!")
+	}
+}
+
+
